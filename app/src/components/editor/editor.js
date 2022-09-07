e0a87e33c246617383e5a20d04f1a7ed82ed4e7d
Rostyslav <rostikbovonenko@gmail.com>
2022-09-07 17:11:40 +0200

    added contentEditable to all text in iframe page with recursy

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,10 +1,11 @@
+import '../../helpers/iframeLoader.js';
 import React, {Component} from "react";
 import axios from "axios";
 
 export default class Editor extends Component {
     constructor() {
         super();
-
+        this.currentPage = "index.html";
         this.state = {
             pageList: [],
             newPageName: ''
@@ -13,7 +14,41 @@ export default class Editor extends Component {
     }
 
     componentDidMount() {
+        this.init(this.currentPage);
+    }
+
+    init(page) {
+        this.iframe = document.querySelector('iframe');
+        this.open(page);
         this.loadPageList();
+    }
+
+    open(page) {
+        this.currentPage = `../${page}`;
+        this.iframe.load(this.currentPage, () => {
+            const body = this.iframe.contentDocument.body;
+            let textNodes = [];
+
+            function recursy(element) {
+                element.childNodes.forEach(node => {
+                    
+                    if(node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, '').length > 0) {
+                        textNodes.push(node);
+                    } else {
+                        recursy(node);
+                    }
+                })
+            };
+
+            recursy(body);
+            
+            textNodes.forEach(node => {
+                const wrapper = this.iframe.contentDocument.createElement('text-editor');
+                node.parentNode.replaceChild(wrapper, node);
+                wrapper.appendChild(node);
+                wrapper.contentEditable = 'true';
+            });
+        })
     }
 
     loadPageList() {
@@ -37,26 +72,27 @@ export default class Editor extends Component {
     }
 
     render() {
-        const {pageList} = this.state;
-        const pages = pageList.map((page, i) => {
-            return (
-                <h1 key={i}>
-                    {page}
-                    <a 
-                        href="#"
-                        onClick={() => this.deletePage(page)}>(x)</a>
-                </h1>
-            )
-        });
+        // const {pageList} = this.state;
+        // const pages = pageList.map((page, i) => {
+        //     return (
+        //         <h1 key={i}>
+        //             {page}
+        //             <a 
+        //                 href="#"
+        //                 onClick={() => this.deletePage(page)}>(x)</a>
+        //         </h1>
+        //     )
+        // });
 
         return(
-            <>
-                <input 
-                    onChange={(e) => {this.setState({newPageName: e.target.value})}} 
-                    type="text" />
-                <button onClick={this.createNewPage}>Create new page</button>
-                {pages}
-            </>
+            <iframe src={this.currentPage} frameBorder="0"></iframe>
+            // <>
+            //     <input 
+            //         onChange={(e) => {this.setState({newPageName: e.target.value})}} 
+            //         type="text" />
+            //     <button onClick={this.createNewPage}>Create new page</button>
+            //     {pages}
+            // </>
         )
     }
 }
--- app/src/main.js
+++ app/src/main.js
@@ -4,9 +4,7 @@ import Editor from './components/editor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
         <Editor/>
-    </React.StrictMode>
 )
 
 // function getPageList() {
