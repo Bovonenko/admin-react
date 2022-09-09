9fece7444093a81bd1f5f910a2ee23a2713b1eb7
Rostyslav <rostikbovonenko@gmail.com>
2022-09-09 12:46:10 +0200

    fixed bags in text editing

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -2,6 +2,7 @@ import '../../helpers/iframeLoader.js';
 import React, {Component} from "react";
 import axios from "axios";
 import DOMHelper from '../../helpers/dom-helper.js';
+import EditorText from '../editor-text';
 
 export default class Editor extends Component {
     constructor() {
@@ -39,28 +40,40 @@ export default class Editor extends Component {
             .then(html => axios.post('./api/saveTempPage.php', {html}))  // creates new page in folder
             .then(() => this.iframe.load('../temp.html'))   // that we can open now in Iframe
             .then(() => this.enableEditing())  // enable editing the page when iframe is ready
+            .then(() => this.injectStyles())
     }
 
     save() {
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
         const html = DOMHelper.serializeDOMToString(newDom);
+        console.log('saved')
         axios
             .post("./api/savePage.php", {pageName: this.currentPage, html})
     }
 
     enableEditing() {
         this.iframe.contentDocument.body.querySelectorAll('text-editor').forEach(element => {
-            element.contentEditable = 'true';     // makes text editable
-            element.addEventListener('input', () => {
-                this.onTextEdit(element);    // synchronize our copies when text is editing
-            })
+            const id = element.getAttribute('nodeid');
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`)
+
+            new EditorText(element, virtualElement); 
         });
     }
 
-    onTextEdit(element) {  
-        const id = element.getAttribute('nodeid');
-        this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
+    injectStyles() {
+        const style = this.iframe.contentDocument.createElement('style');
+        style.innerHTML = `
+            text-editor:hover {
+                outline: 3px solid orange;
+                outline-offset: 8px;
+            }
+            text-editor:focus {
+                outline: 3px solid red;
+                outline-offset: 8px;
+            }
+        `;
+        this.iframe.contentDocument.head.appendChild(style);
     }
 
     loadPageList() {
